fe9f23ae0dc743a2fdd70b585ed464966eb27400
MrMorham <g7058190@gmail.com>
2021-09-16 22:46:21 +0100

    Updates

--- static/js/main.e60696f0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmr-morhams-website"]=this["webpackJsonpmr-morhams-website"]||[]).push([[0],{9:function(e,t,r){"use strict";r.r(t);var n=r(0),s=r.n(n),c=r(3),i=r.n(c),o=r(1);var a=function(){return Object(o.jsx)("div",{children:"HELLO WORLD"})};i.a.render(Object(o.jsx)(s.a.StrictMode,{children:Object(o.jsx)(a,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.e60696f0.chunk.js.map
